61989b56ffd5a1ecc48de7969cea32c2c38c53ab
imranhossainemi <blur-imran@hotmail.com>
2023-01-31 15:24:59 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const menuBtn = document.querySelector('.mobile-nav-btm');
 const navigation = document.querySelector('.navigation');
-
+const logo = document.querySelector('.logo');
+const navSec = document.querySelector('.navSec');
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active');
   navigation.classList.toggle('active');
+  logo.classList.toggle('hide');
+  navSec.classList.toggle('navSec2');
 });
-
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   menuBtn.classList.remove('active');
   navigation.classList.remove('active');
